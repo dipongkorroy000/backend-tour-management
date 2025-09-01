ce8785e81895f8980633fd4058308043223e9eee
Dipongkor Roy <dipongkorroy000@gmail.com>
2025-09-01 10:20:32 +0600

    google auth setup completed

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,16 @@ import cookieParser from "cookie-parser";
 import { router } from "./app/routes";
 import { globalErrorHandler } from "./app/middlewares/globalErrorhandler";
 import notFound from "./app/middlewares/notFound";
+import passport from "passport";
+import expressSession from "express-session";
 
 const app: Application = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(expressSession({ secret: "Your secret", resave: false, saveUninitialized: false }));
 
 app.use("/api/v1", router);
 
---
+++ src/app/config/passport.ts
@@ -0,0 +1,49 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy, Profile, VerifyCallback } from "passport-google-oauth20";
+import { envVars } from "./env";
+import { User } from "../modules/user/user.model";
+import { Role } from "../modules/user/user.interface";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: envVars.GOOGLE_CLIENT_ID,
+      clientSecret: envVars.GOOGLE_CLIENT_SECRET,
+      callbackURL: envVars.GOOGLE_CALLBACK_URL,
+    },
+    async (accessToken: string, refrestToken: string, profile: Profile, done: VerifyCallback) => {
+      try {
+        const email = profile.emails?.[0].value;
+
+        if (!email) {
+          return done(null, false, { message: "No Email Found" });
+        }
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+          user = await User.create({
+            email,
+            name: profile.displayName,
+            picture: profile.photos?.[0].value,
+            role: Role.USER,
+            isVerified: true,
+            auths: [{ provider: "google", providerId: profile.id }],
+          });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        console.log("Google Strategy Error", err);
+        return done(err);
+      }
+    }
+  )
+);
+
+// frontend localhost : 5173 -> localhost:5000/api/v1/auth/google -> passport -> Google OAuth Constant -> gmail login -> successful -> callback url: localhost:5000/api/v1/auth/google/callback
+
+// Bridge -----
+// Custom -> email, password, role: USER , name ..... -> registration -> DB
+
+// Google -> req -> google -> successful : JWT Token : Role, email, -> DB -store -> token - api access
